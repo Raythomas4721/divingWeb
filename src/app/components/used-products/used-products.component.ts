61bfdcebc9b49240f1c5bca56d486387ad3d9562
Chloe <xian19991023@gmail.com>
2025-02-11 17:47:02 +0800

    改畫面

--- src/app/components/used-products/used-products.component.ts
+++ src/app/components/used-products/used-products.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserDTO } from 'src/app/interface/userDTO';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-used-products',
@@ -7,5 +10,18 @@ import { Component } from '@angular/core';
 
 })
 export class UsedProductsComponent {
+  constructor(private authService: AuthService, private userService: UserService) { }
+
+  user?: UserDTO | null;
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.user = user?.user;
+      if (user) {
+        console.log("用戶資料已載入:", this.user);
+      } else {
+        console.log("等待 API 返回，用戶資料尚未載入");
+      }
+    });
+  }
 
 }
