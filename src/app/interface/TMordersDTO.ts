90ff4b11736af082601b79e971b3fe46bceeac54
Ray <raywork113@gmail.com>
2025-02-28 20:55:03 +0800

    更新profile orders頁面

--- src/app/interface/TMordersDTO.ts
+++ src/app/interface/TMordersDTO.ts
@@ -1,12 +1,19 @@
 export interface TMordersDTO {
   orderType: string;
   orderId: number;
-  orderStatus?: string | null;
-  shipAddress?: string | null;
-  shipPhone?: string | null;
-  paymentMethod?: string | null;
-  orderStatusId?: number | null;
-  orderLogId?: number | null;
-  orderDate?: string | null;
-  siteId?: number | null;
+  orderStatus: string | null;
+  shipAddress: string | null;
+  shipPhone: string | null;
+  paymentMethod: string | null;
+  orderStatusId: number | null;
+  orderLogId: number | null;
+  orderDate: string | null;
+  siteId: number | null;
+  details: OrderDetailDTO[];
+}
+
+export interface OrderDetailDTO {
+  itemName: string;
+  quantity: number;
+  totalPrice: number;
 }
